07aafaa3028825c4ebbc2ff75a77ad1753267d75
SevvalKolaca <sevval.kolaca.00@gmail.com>
2025-01-30 09:48:41 +0300

    CRM - cqrs versiyon oluşturuldu.

---
+++ crm.cqrs/src/main/java/crm/cqrs/crm/cqrs/Application.java
@@ -0,0 +1,13 @@
+package crm.cqrs.crm.cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
